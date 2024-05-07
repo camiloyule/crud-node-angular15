902812787ce15891b2d9148139d8532f3c61c0bf
Camilo Yule <camilo.yule@gmail.com>
2024-05-07 01:22:04 -0500

    cors added

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import swaggerUi from 'swagger-ui-express';
 import itemRoutes from './routes/itemRoutes';
 import errorHandler from './utils/errorHandler';
 import sequelize from './config/database';
+import cors from 'cors';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -35,6 +36,14 @@ const specs = swaggerJsdoc(options);
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
 
 app.use(express.json());
+
+//CORS CONF
+const corsOptions={
+    origin: ['http://localhost:4200','http://localhost:3000'],
+    // credentials: true, //
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
 
 // Routes
 app.use('/items', itemRoutes);
